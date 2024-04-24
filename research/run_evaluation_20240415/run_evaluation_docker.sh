3626fca7fbd0a80a9bdc5b3de245f7446e43a5b0
Evgeny Saveliev <e.s.saveliev@gmail.com>
2024-04-24 14:29:18 +0100

    Add all the files

---
+++ research/run_evaluation_20240415/run_evaluation_docker.sh
@@ -0,0 +1,5 @@
+python /home/SWE-bench/swebench/harness/run_evaluation.py \
+    --predictions_path /home/SWE-bench/wd/run_evaluation_20240415/swe-bench-example-preds.json \
+    --swe_bench_tasks /home/SWE-bench/wd/reused/swe-bench.json \
+    --log_dir /home/SWE-bench/wd/run_evaluation_20240415/logs \
+    --testbed /home/SWE-bench/wd/run_evaluation_20240415/testbed
