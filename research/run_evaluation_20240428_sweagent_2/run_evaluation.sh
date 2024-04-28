4bdbd682b38adf86cbdaa71929379f71e25d351d
Evgeny Saveliev <e.s.saveliev@gmail.com>
2024-04-28 18:11:03 +0000

    Add more stuff

---
+++ research/run_evaluation_20240428_sweagent_2/run_evaluation.sh
@@ -0,0 +1,5 @@
+python /home/user/SWE-bench/swebench/harness/run_evaluation.py \
+    --predictions_path /home/user/SWE-bench/research/run_evaluation_20240428_sweagent_2/all_preds.json \
+    --swe_bench_tasks /home/user/SWE-bench/research/run_evaluation_20240428_sweagent_2/swe-bench-filtered.json \
+    --log_dir /home/user/SWE-bench/research/run_evaluation_20240428_sweagent_2/logs \
+    --testbed /home/user/SWE-bench/research/run_evaluation_20240428_sweagent_2/testbed
