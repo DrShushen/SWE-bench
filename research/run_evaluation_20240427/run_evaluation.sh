cfd2da9235366aa32df6f47abd90622eca2dffc9
Evgeny Saveliev <e.s.saveliev@gmail.com>
2024-04-28 13:08:13 +0000

    Update paths

--- research/run_evaluation_20240427/run_evaluation.sh
+++ research/run_evaluation_20240427/run_evaluation.sh
@@ -1,5 +1,5 @@
 python /home/user/SWE-bench/swebench/harness/run_evaluation.py \
-    --predictions_path /home/user/SWE-bench/wd/run_evaluation_20240427/swe-bench-example-preds.json \
-    --swe_bench_tasks /home/user/SWE-bench/wd/reused/swe-bench.json \
-    --log_dir /home/user/SWE-bench/wd/run_evaluation_20240427/logs \
-    --testbed /home/user/SWE-bench/wd/run_evaluation_20240427/testbed
+    --predictions_path /home/user/SWE-bench/research/run_evaluation_20240427/swe-bench-example-preds.json \
+    --swe_bench_tasks /home/user/SWE-bench/research/reused/swe-bench.json \
+    --log_dir /home/user/SWE-bench/research/run_evaluation_20240427/logs \
+    --testbed /home/user/SWE-bench/research/run_evaluation_20240427/testbed
