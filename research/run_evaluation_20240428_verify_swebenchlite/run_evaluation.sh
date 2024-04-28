0729b5800e076a6608190d053bca6043f7181d96
Evgeny Saveliev <e.s.saveliev@gmail.com>
2024-04-28 23:38:39 +0000

    Add run to check SWEBench Lite

---
+++ research/run_evaluation_20240428_verify_swebenchlite/run_evaluation.sh
@@ -0,0 +1,5 @@
+python /home/user/SWE-bench/swebench/harness/run_evaluation.py \
+    --predictions_path /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/fake_solutions.json \
+    --swe_bench_tasks /home/user/SWE-bench/research/datasets/SWE-bench_Lite/test-00000-of-00001.json \
+    --log_dir /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/logs \
+    --testbed /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/testbed
