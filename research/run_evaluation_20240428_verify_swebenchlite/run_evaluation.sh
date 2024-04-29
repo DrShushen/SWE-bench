b381726cd0fdb70d8f2a30ec85e725e185e9a2b4
Evgeny Saveliev <e.s.saveliev@gmail.com>
2024-04-29 04:36:51 +0100

    Fix script

--- research/run_evaluation_20240428_verify_swebenchlite/run_evaluation.sh
+++ research/run_evaluation_20240428_verify_swebenchlite/run_evaluation.sh
@@ -22,7 +22,7 @@ python /home/user/SWE-bench/swebench/harness/run_evaluation.py \
     --log_dir /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/logs \
     --testbed /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/testbed
 python /home/user/SWE-bench/swebench/harness/run_evaluation.py \
-    --predictions_path /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/fake_solutions_PART.json \
+    --predictions_path /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/fake_solutions_WORKING.json \
     --swe_bench_tasks /home/user/SWE-bench/research/datasets/swe-bench.json \
     --log_dir /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/logs \
     --testbed /home/user/SWE-bench/research/run_evaluation_20240428_verify_swebenchlite/testbed \
